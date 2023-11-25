006c3ee2beef03960711dd2bd3817847eaba7928
S554271 <145793094+S554271@users.noreply.github.com>
2023-11-25 15:23:31 -0600

    added abstract class player

---
+++ src/project02startingfiles/Player.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project02startingfiles;
+
+/**
+ *
+ * @author S554271
+ */
+public abstract class Player {
+
+}
