d81403a8a3e6d725a479bb6a0a2f39aa34de0676
S554271 <145793094+S554271@users.noreply.github.com>
2023-11-28 22:40:54 -0600

    added comments

--- src/project02startingfiles/Project02StartingFiles.java
+++ src/project02startingfiles/Project02StartingFiles.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
  */
 public class Project02StartingFiles {
 
+    // Creating a Scanner and Random object for user input and random events
     private static Scanner scanner = new Scanner(System.in);
     private static Random random = new Random();
 
@@ -28,9 +29,11 @@ public class Project02StartingFiles {
         System.out.println("(k)Knight I| (h)Healer I| (w)wizard || (t)Thief");
         System.out.print(">>");
 
+        // Reading the user's choice for character selection
         char choice = scanner.next().toLowerCase().charAt(0);
         Player player;
 
+        // Creating a player object based on the user's choice
         switch (choice) {
             case 'k':
                 player = new Knight();
@@ -49,18 +52,21 @@ public class Project02StartingFiles {
                 return;
         }
 
+        // Printing a welcome message for the chosen character and starting the game
         System.out.println("\nWelcome, " + player.getClass().getSimpleName() + "!");
         playGame(player);
     }
 
+    // Method to handle the main game loop
     private static void playGame(Player player) {
         while (player.getHealth() > 0) {
             System.out.println("\nWhat would you like to do?");
             System.out.println("(?) Status Report || (n) Move North || (s) Move South || (e) Move East || (w) Move West || (q) Quit");
             System.out.print(">>");
 
+            // Reading the player's action choice
             char action = scanner.next().toLowerCase().charAt(0);
-
+            //performing the chosen action
             switch (action) {
                 case '?':
                     System.out.println(player);
@@ -80,6 +86,7 @@ public class Project02StartingFiles {
         }
     }
 
+    // Method to handle player movement
     private static void move(Player player, char direction) {
         int scenario = random.nextInt(5); // 20% chance of foe attack
 
@@ -94,11 +101,13 @@ public class Project02StartingFiles {
         }
     }
 
+    // Method to get a description of the current scene based on the scenario
     private static String getSceneDescription(int scenario) {
         String[] scenes = {"Nothing here...", "Nice trees around here...", "Interesting cottage there...", "Potty break..."};
         return scenes[scenario - 1];
     }
 
+    // Method to get the type of foe based on the scenario
     private static String getFoe(int scenario) {
         String[] foes = {"zombie", "bandit", "lobbyist"};
 
@@ -108,10 +117,12 @@ public class Project02StartingFiles {
         return foes[adjustedScenario];
     }
 
+    // Method to handle the player's response to a foe attack
     private static void handleFoeAttack(Player player) {
         System.out.println("How would you like to handle this?");
         System.out.println("(s) Special Move || (r) Run");
 
+        // Read the player's choice
         char choice = scanner.next().toLowerCase().charAt(0);
 
         if (choice == 's') {
@@ -135,16 +146,19 @@ public class Project02StartingFiles {
             System.out.println("Invalid choice. Please try again.");
             handleFoeAttack(player);
         }
-
+        // Displaying player status after the encounter
         System.out.println(player);
 
+        // Checking if the player's health is zero or below to end the game
         if (player.getHealth() <= 0) {
             endGame(player);
         }
     }
 
+    // Method to display the final stats and end the game
     private static void endGame(Player player) {
-        System.out.println("\nThe game has come to an end! Your final stats:");
+        System.out.println("****************************************************\n****************************************************");
+        System.out.println("The game has come to an end! Your final stats:\n");
         System.out.println(player);
         System.out.println("Thanks for playing!");
     }
